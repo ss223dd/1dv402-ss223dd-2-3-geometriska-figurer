783ad812905bc90a79ea8f59d6aa734ce69cd41b
Sebastian Ström <ss223dd@student.lnu.se>
2014-10-18 19:08:21 +0200

    Initial commit

---
+++ 1DV402.S2.L03A/1DV402.S2.L03A/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L03A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Geometriska Figurer - Nivå A";
+        }
+    }
+}
